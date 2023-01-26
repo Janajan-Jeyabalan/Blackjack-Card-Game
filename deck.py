267c20ae4a5e0cfae8b6ef4b52f51e252cd23f80
JanajanJ <janajanjeyabalan@gmail.com>
2023-01-26 18:49:05 -0500

    Update deck.py

--- deck.py
+++ deck.py
@@ -7,3 +7,9 @@ class Deck:
         self.cards = []
         self.create_deck()
         self.shuffle()
+
+    def create_deck(self):
+        for suit in range(4):
+            for card in range(1, 14):
+                new_card = Card(suit, card)
+                self.cards.append(new_card)
