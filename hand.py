b09a5a53360da1e5f908afe7b2bd1b4b05436227
JanajanJ <janajanjeyabalan@gmail.com>
2023-01-26 18:53:17 -0500

    Update hand.py

--- hand.py
+++ hand.py
@@ -5,3 +5,10 @@ class Hand:
     def get_value(self):
         value = 0
         aces = 0
+
+        for card in self.cards:
+            val = card.value
+            if val == 1:
+                aces += 1
+            else:
+                value += min(val, 10)
