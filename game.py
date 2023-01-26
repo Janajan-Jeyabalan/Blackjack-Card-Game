feae622ddc85a58ef8545264d00b37656f483c6a
JanajanJ <janajanjeyabalan@gmail.com>
2023-01-26 18:56:28 -0500

    Update game.py

--- game.py
+++ game.py
@@ -4,3 +4,9 @@ from hand import Hand
 
 class Game:
     MINIMUM_BET = 1
+
+    def __init__(self, player, dealer):
+        self.player = player
+        self.dealer = dealer
+        self.bet = None
+        self.deck = Deck()
